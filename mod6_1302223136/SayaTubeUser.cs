9028e470247716e703973b78040eb1bfb5a67498
iniarvinhehe <iniarvinhehe@gmail.com>
2024-03-27 08:20:50 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- mod6_1302223136/SayaTubeUser.cs
+++ mod6_1302223136/SayaTubeUser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,39 @@ namespace mod6_1302223136
 {
     internal class SayaTubeUser
     {
+        private int id;
+        private List<SayaTubeVideo> uploadedVideos;
+        public string username;
+
+        public SayaTubeUser(string username)
+        {
+            this.username = username;
+            uploadedVideos = new List<SayaTubeVideo>();
+        }
+
+        public int GetTotalVideoPlayCount()
+        {
+            int sum = 0;
+            for (int i = 0; i < uploadedVideos.Count; i++)
+            {
+                sum += uploadedVideos[i].GetPlayCount();
+            }
+            return sum;
+        }
+
+        public void AddVideo(SayaTubeVideo a)
+        {
+            uploadedVideos.Add(a);
+        }
+
+        public void PrintAllVideoPlaycount()
+        {
+            System.Console.WriteLine("User:" + username);
+            for (int i = 0; i < uploadedVideos.Count; i++)
+            {
+                System.Console.WriteLine("Video " + (i+1) + " judul: " + uploadedVideos[i].GetTitle());
+            }
+
+        }
     }
 }
