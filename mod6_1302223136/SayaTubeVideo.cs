340349b4a9c6add14926c6811521eeea89c17c9e
iniarvinhehe <iniarvinhehe@gmail.com>
2024-03-27 07:11:53 +0700

    first commit

---
+++ mod6_1302223136/SayaTubeVideo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod6_1302223136
+{
+    internal class SayaTubeVideo
+    {
+    }
+}
